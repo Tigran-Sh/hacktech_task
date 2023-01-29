4f3e4569e3e954a75006559f2cfd3d49d5b1264a
Tigran Shelelenkyan <tigran.shelelenkyan@gmail.com>
2023-01-29 22:32:09 +0400

    add file input to swagger

--- data_upload/views.py
+++ data_upload/views.py
@@ -1,8 +1,12 @@
 from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.parsers import MultiPartParser, FormParser, FileUploadParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 
 from data_upload.serializers import PersonSerializer
 
@@ -12,6 +16,17 @@ class FileView(APIView):
     permission_classes = (IsAuthenticated,)
     serializer_class = PersonSerializer
 
+    @swagger_auto_schema(
+        operation_description='Upload EXCEL file with headers name, email, phone_number',
+        manual_parameters=[openapi.Parameter(
+            name="file",
+            in_=openapi.IN_FORM,
+            type=openapi.TYPE_FILE,
+            required=True,
+            description="File"
+        )],
+    )
+    @action(detail=False, methods=['post'], parser_classes=(FormParser,))
     def post(self, request, *args, **kwargs):
         file_obj = request.data['file']
         file_serializer = self.serializer_class(data=request.data, context={"file_obj": file_obj})
